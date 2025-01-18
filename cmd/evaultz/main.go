d4d42b33c082b4e9b720f8427976a306b4bec40a
Aadil-Nabi <aadilnabi99@outlook.com>
2025-01-18 15:08:40 +0530

    gin framework added

--- cmd/evaultz/main.go
+++ cmd/evaultz/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("main function")
+
+	router := gin.Default()
+
+	router.GET("/", homePage)
+	router.Run("localhost:9000")
+}
+
+func homePage(c *gin.Context) {
+	c.String(http.StatusOK, "This is my HomePage")
 }
