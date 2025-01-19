397ccdd8539d058021d7aa3d59aa4b83ea150439
Aadil-Nabi <aadilnabi99@outlook.com>
2025-01-19 22:06:04 +0530

    gin updated

--- cmd/evaultz/main.go
+++ cmd/evaultz/main.go
@@ -10,12 +10,21 @@ import (
 func main() {
 	fmt.Println("main function")
 
+	// Create Gin Router.
 	router := gin.Default()
 
+	// Register Routes
 	router.GET("/", homePage)
+	router.GET("/user/:name", getUserName)
+	// Start a Server.
 	router.Run("localhost:9000")
 }
 
 func homePage(c *gin.Context) {
 	c.String(http.StatusOK, "This is my HomePage")
+}
+
+func getUserName(c *gin.Context) {
+	name := c.Param("name")
+	c.String(http.StatusOK, "Hello %s ", name)
 }
